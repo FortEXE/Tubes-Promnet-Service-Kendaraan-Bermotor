ce95184e76e0a80dfe26cbd2a34df07f45824426
Adnan Khairi <squadron296@gmail.com>
2018-05-18 05:49:44 +0700

    update model

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/tp3/';
+$config['base_url'] = 'http://localhost:80/tubespromnet/';
 
 /*
 |--------------------------------------------------------------------------
